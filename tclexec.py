507a9fc9e9e70a046ac79fe93049814fa1f1b144
Denis Isaev <d.isaev@corp.mail.ru>
2015-05-26 21:47:01 +0300

    Ast build.

--- tclexec.py
+++ tclexec.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python3
 import sys
 from lexer import TclLexer, LexerException
+from parser import build_ast
 
 if __name__ == '__main__':
     data = sys.stdin.read()
     lexer = TclLexer(data)
+    tokens = None
     try:
-        for token in lexer.get_tokens():
+        tokens = list(lexer.get_tokens())
+        for token in tokens:
             print(token)
     except LexerException as e:
         sys.stderr.write('invalid source code: {}\n'.format(e))
+    ast = build_ast(tokens)
+    print(ast)
+
